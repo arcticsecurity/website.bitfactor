74e035a508888190e58169e97a6b3b6196ac5273
Roope Tapaninen <roope.tapaninen@bitfactor.fi>
2018-10-04 09:11:45 +0300

    Close dropdown on click outside.

--- js/navburger.js
+++ js/navburger.js
@@ -1,10 +1,19 @@
 var $navBurger = $('#navigation-burger')
 var $navWrapper = $('.navigation__wrapper')
+var $navPages = $('.navigation__pages')
 var menuOpen = 'menu-open'
 var menuClosed = 'menu-closed'
 
-$navBurger.on('click', function() {
+$navBurger.on('click', function(e) {
+  e.stopPropagation();
   if (!$navWrapper.hasClass(menuOpen)) {
     $navWrapper.removeClass(menuClosed).addClass(menuOpen)
   } else $navWrapper.removeClass(menuOpen).addClass(menuClosed)
 })
+
+$navPages.on('click', function(e) {e.stopPropagation()})
+
+$(document).click(function() {
+  if ($navWrapper.hasClass(menuOpen)) $navWrapper.removeClass(menuOpen).addClass(menuClosed)
+})
+
