8583f8e6c3a2033acb11f4f756af24b5e19c9f27
Roope Tapaninen <roope.tapaninen@bitfactor.fi>
2018-10-09 09:01:41 +0300

    Basic responsivity to blog page

--- js/blog-filter.js
+++ js/blog-filter.js
@@ -1,27 +1,34 @@
 var filterHide = 'post__filter-hide'
-var filterDisplayNone = 'post__filter-invisible'
 var filterActiveClass = 'blog-page__filter-active'
-$('.blog-filter__button').on('click', function() {
-  var filterCategory = $(this).data('category')
 
-  $('.post__card').each(function() {
-    var $card = $(this)
-    if ($card.data('category') == filterCategory) {
-      $card.addClass(filterHide)
-      setTimeout(function() {
-        $card.addClass(filterDisplayNone)
-      }, 600)
-    } else {
-      $card.removeClass(filterHide).removeClass(filterDisplayNone)
-    }
-  })
+$('.blog-filter__button').on('click', function() {
+  var $button = $(this)
+
+  if (!$button.hasClass(filterActiveClass)) $button.addClass(filterActiveClass)
+  else $button.removeClass(filterActiveClass)
+
+  filterBlogCards();
+})
+
+function filterBlogCards() {
+  var filters = []
 
   $('.blog-filter__button').each(function() {
     var $button = $(this)
-    if ($button.data('category') == filterCategory) {
-      $button.addClass(filterActiveClass)
-    } else {
-      $button.removeClass(filterActiveClass)
-    }
+    if ($button.hasClass(filterActiveClass)) filters.push($button.data('category'))
   })
-})
+
+  if (filters.length > 0) {
+    $('.post__card').each(function() {
+      var $card = $(this)
+      if (filters.indexOf($card.data('category')) !== -1) {
+        $card.removeClass(filterHide)
+      } else if (!$card.hasClass(filterHide)) {
+        $card.addClass(filterHide)
+      }
+    })
+  } else {
+    $('.post__card').removeClass(filterHide)
+  }
+  
+}
