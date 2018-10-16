033a8b5f38a2df4db248507732318f206fa25d06
Roope Tapaninen <roope.tapaninen@bitfactor.fi>
2018-10-16 16:17:14 +0300

    Contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,1 +1,31 @@
 
+/* checkboxes */
+var $checkbox = $('.contact-form__fake-checkbox')
+
+var checkedClass = 'contact-form__checked'
+var errorClass = 'contact-form__error'
+
+$checkbox.on('click', function() {
+  var $element = $(this)
+  var forName = $element.data('for')
+  var $realCheckbox = $('input[type="checkbox"][name="' + forName + '"]')
+  
+  if ($element.hasClass(checkedClass)) {
+    $element.removeClass(checkedClass)
+    $realCheckbox.prop('checked', false)
+  } else {
+    $element.addClass(checkedClass)
+    $realCheckbox.prop('checked', true)
+  }
+})
+
+/* submit */
+$('#contact-form__submit-button').on('click', function() {
+  $('#contact-form__hidden-submit').click()
+})
+
+$('#contact-form').on('submit', function(e) {
+  e.preventDefault()
+
+  console.log($(this))
+})
