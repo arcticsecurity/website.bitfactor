e5135e80441c1c8e39339ac393da7394e7d317c0
Roope Tapaninen <roope.tapaninen@bitfactor.fi>
2018-10-22 16:28:30 +0300

    frontpage

--- js/script.js
+++ js/script.js
@@ -31,3 +31,46 @@ $('#news-page__loadmore').on('click', function() {
     }
   }.bind(this), 500)
 })
+
+
+$('.frontpage-expander__show-more').on('click', function() {
+  var hideClass = 'frontpage-expander__hide'
+
+  var $element = $(this)
+  var tag = $element.data('tag')
+
+  $('.frontpage-expander[data-tag="' + tag + '"]').css('height', 'auto')
+  var oldHeight = $('.frontpage-expander[data-tag="' + tag + '"]').height()
+
+  $('.frontpage-expander__show-less[data-tag="' + tag + '"]').removeClass(hideClass)
+  $('.frontpage-expander__content[data-tag="' + tag + '"]').removeClass(hideClass)
+
+  var newHeight = $('.frontpage-expander[data-tag="' + tag + '"]').height()
+
+  autoHeightAnimate($('.frontpage-expander[data-tag="' + tag + '"]'), 500, oldHeight, newHeight + 50)
+
+  $(this).addClass(hideClass);
+})
+
+$('.frontpage-expander__show-less').on('click', function() {
+  var hideClass = 'frontpage-expander__hide'
+
+  var $element = $(this)
+  var tag = $element.data('tag')
+
+  $('.frontpage-expander[data-tag="' + tag + '"]').css('height', 'auto')
+  var oldHeight = $('.frontpage-expander[data-tag="' + tag + '"]').height()
+
+  $('.frontpage-expander__show-more[data-tag="' + tag + '"]').removeClass(hideClass)
+  $('.frontpage-expander__content[data-tag="' + tag + '"]').addClass(hideClass)
+
+  var newHeight = $('.frontpage-expander[data-tag="' + tag + '"]').height()
+  autoHeightAnimate($('.frontpage-expander[data-tag="' + tag + '"]'), 500, oldHeight, newHeight + 50)
+
+  $(this).addClass(hideClass)
+})
+
+function autoHeightAnimate(element, time, oldHeight, newHeight) {
+  element.height(oldHeight)
+  element.stop().animate({ height: newHeight }, time)
+}
