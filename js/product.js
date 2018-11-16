895046e034c9ae24e6f1445469910585d3b10744
Roope Tapaninen <roope.tapaninen@bitfactor.fi>
2018-11-16 12:37:10 +0200

    Hub

--- js/product.js
+++ js/product.js
@@ -8,13 +8,27 @@
 
   $arrowLeft.on('click', function() {
     var $activeSlide = $mobileSlides.filter(activeSlide).removeClass(activeSlideClass)
-    if ($activeSlide.prev().hasClass(mobileSlideClass)) $activeSlide.prev().addClass(activeSlideClass)
-    else $mobileSlides.last().addClass(activeSlideClass)
+    $('.product__slider-content__image[data-slider-image="' + $activeSlide.data('slider-image') + '"]').removeClass(activeSlideClass)
+    var $nextActiveSlide;
+    if ($activeSlide.prev().hasClass(mobileSlideClass)) {
+      $nextActiveSlide = $activeSlide.prev().addClass(activeSlideClass)
+    } else {
+      $nextActiveSlide = $mobileSlides.last().addClass(activeSlideClass)
+    }
+
+    $('.product__slider-content__image[data-slider-image="' + $nextActiveSlide.data('slider-image') + '"]').addClass(activeSlideClass)
   })
   $arrowRight.on('click', function() {
     var $activeSlide = $mobileSlides.filter(activeSlide).removeClass(activeSlideClass)
-    if ($activeSlide.next().hasClass(mobileSlideClass)) $activeSlide.next().addClass(activeSlideClass)
-    else $mobileSlides.first().addClass(activeSlideClass)
+    $('.product__slider-content__image[data-slider-image="' + $activeSlide.data('slider-image') + '"]').removeClass(activeSlideClass)
+    var $nextActiveSlide;
+    if ($activeSlide.next().hasClass(mobileSlideClass)) {
+      $nextActiveSlide = $activeSlide.next().addClass(activeSlideClass)
+    } else {
+      $nextActiveSlide = $mobileSlides.first().addClass(activeSlideClass)
+    }
+
+    $('.product__slider-content__image[data-slider-image="' + $nextActiveSlide.data('slider-image') + '"]').addClass(activeSlideClass)
   })
 
 
@@ -24,7 +38,7 @@
     desktopNav.append('<a class="product__nav-item" href="#' + $(this).attr('id') + '">' + $(this).data('nav') + '</a>')
   })
 
-  $(window).on("scroll", function() {
+  $(window).on("scroll, resize", function() {
     let fromTop = window.scrollY + 180
   
     $('.product__nav-item').each(function() {
@@ -34,6 +48,15 @@
         $section.offset().top + $section.height() > fromTop)
         $(this).addClass("current")
       else $(this).removeClass("current")
+    })
+
+    $('.product__info-section').each(function() {
+      var $content = $(this)
+      var $image = $('.product__slider-content__image[data-slider-image="' + $content.data('slider-image') + '"]')
+
+      if ($content.offset().top - 200 <= fromTop) {
+        $image.addClass("current")
+      } else $image.removeClass("current")
     })
   })
 
